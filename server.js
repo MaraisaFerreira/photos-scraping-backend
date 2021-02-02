a3857f5a36b8668dfda5f5b9e1ff7c63f65a12ca
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-02-02 10:40:52 -0300

    server update

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const scraping = require('./web-scraping');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET');
+	res.setHeader('Access-Control-Allow-Headers', 'content-type');
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
+
+app.listen(9090, () => {
+	console.log('WebServer funcionando na porta 9090...');
+});
+
+/* retorna os links das fotos */
+app.get('/nasa-photos', async (req, res) => {
+	await scraping.scraping();
+	fs.readFile('imgs_source.json', 'utf8', (err, data) => {
+		if (err) {
+			let response = { status: 'Erro!', result: err };
+			res.json(response);
+		} else {
+			let obj = JSON.parse(data);
+
+			let result = obj ? obj : 'Nada encontrado.';
+
+			let response = { status: 'Sucesso!', result: result };
+			res.json(response);
+		}
+	});
+});
